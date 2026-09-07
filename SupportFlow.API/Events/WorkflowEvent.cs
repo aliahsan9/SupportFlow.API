08319c02ee15b3dd333beb55ebb506e956fb626e
Ali Ahsan <aliahsan8751@gmail.com>
2026-09-07 17:22:46 +0500

    added workflow event

---
+++ SupportFlow.API/Events/WorkflowEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace SupportFlow.API.Events;
+
+public sealed class WorkflowEvent
+{
+    public WorkflowEventType Type { get; init; }
+
+    public string WorkflowId { get; init; } = string.Empty;
+
+    public string? ExecutorName { get; init; }
+
+    public string? Message { get; init; }
+
+    public DateTime TimestampUtc { get; init; } = DateTime.UtcNow;
+}
---
+++ SupportFlow.API/Events/WorkflowEventType.cs
@@ -0,0 +1,18 @@
+﻿namespace SupportFlow.API.Events;
+
+public enum WorkflowEventType
+{
+    WorkflowStarted,
+
+    WorkflowCompleted,
+
+    WorkflowFailed,
+
+    ExecutorStarted,
+
+    ExecutorCompleted,
+
+    ExecutorFailed,
+
+    RoutingDecision
+}
