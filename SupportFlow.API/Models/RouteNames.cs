ec58b7fec841eb31f2eaa12ae4ec9320220d9067
Ali Ahsan <aliahsan8751@gmail.com>
2026-09-07 06:43:42 +0500

    added edges and conditional routing

---
+++ SupportFlow.API/Models/RouteNames.cs
@@ -0,0 +1,8 @@
+﻿namespace SupportFlow.API.Models;
+
+public static class RouteNames
+{
+    public const string Support = "support";
+    public const string Billing = "billing";
+    public const string Technical = "technical";
+}
