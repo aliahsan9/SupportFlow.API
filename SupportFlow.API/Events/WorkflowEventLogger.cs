116889ae05d418662230fa62705c7d75a0ba98d5
Ali Ahsan <aliahsan8751@gmail.com>
2026-09-07 17:25:50 +0500

    added workflow event

---
+++ SupportFlow.API/Events/WorkflowEventLogger.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SupportFlow.API.Events;
+
+public sealed class WorkflowEventLogger
+{
+    private readonly ILogger<WorkflowEventLogger> _logger;
+
+    public WorkflowEventLogger(
+        ILogger<WorkflowEventLogger> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Publish(WorkflowEvent workflowEvent)
+    {
+        _logger.LogInformation(
+            "Workflow Event | Type: {Type} | WorkflowId: {WorkflowId} | Executor: {ExecutorName} | Message: {Message} | Timestamp: {TimestampUtc}",
+            workflowEvent.Type,
+            workflowEvent.WorkflowId,
+            workflowEvent.ExecutorName,
+            workflowEvent.Message,
+            workflowEvent.TimestampUtc);
+    }
+}
